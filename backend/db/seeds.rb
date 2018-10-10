60dfcf341ae7314199335c0b637c1e56a089ad7f
Christopher Arnold <chrisarnold2010@gmail.com>
2018-10-10 17:27:08 -0500

    added seeds destroy_all

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Note.destroy_all
+User.destroy_all
+
 chris = User.create(name: "arnie")
 janett = User.create(name: "janey")
 doug = User.create(name: "dougie")

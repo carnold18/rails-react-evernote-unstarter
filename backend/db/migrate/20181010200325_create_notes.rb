2a475ef9c60e6effe3d9187cd92c9b868d340131
Christopher Arnold <chrisarnold2010@gmail.com>
2018-10-10 16:57:31 -0500

    seeds added and routes resources created

--- backend/db/migrate/20181010200325_create_notes.rb
+++ backend/db/migrate/20181010200325_create_notes.rb
@@ -3,6 +3,7 @@ class CreateNotes < ActiveRecord::Migration[5.2]
     create_table :notes do |t|
       t.string :title
       t.string :body
+      t.belongs_to :user
 
       t.timestamps
     end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+chris = User.create(name: "arnie")
+janett = User.create(name: "janey")
+doug = User.create(name: "dougie")
+
+groceries = Note.create(title: "Grocery List", body: "Bananas, Eggs, Cheese", user_id: 1)
+practice = Note.create(title: "Guitar Notes", body: "need to remember to buy new pic in the morning", user_id: 2)
+television = Note.create(title: "Shows", body: "GOT, AHS, SYTYCD", user_id: 1)

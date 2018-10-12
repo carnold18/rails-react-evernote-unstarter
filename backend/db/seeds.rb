14d655a276c74c2b56fe8a2c32944fbf243dd923
Christopher Arnold <chrisarnold2010@gmail.com>
2018-10-12 10:18:47 -0500

    updated seeds to json format

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,10 +9,55 @@
 Route.destroy_all
 User.destroy_all
 
-chris = User.create(username: "arnie", email: "chris@example.com", password: "example")
-janett = User.create(username: "janey", email: "janey@example.com", password: "example")
-doug = User.create(username: "dougie", email: "dougie@example.com", password: "example")
+# chris = User.create(username: "arnie", email: "chris@example.com", password: "example")
+# janett = User.create(username: "janey", email: "janey@example.com", password: "example")
+# doug = User.create(username: "dougie", email: "dougie@example.com", password: "example")
 
-short = Route.create(transportation: "moto", distance: 200, name: "Can Tho to Ho Chi Minh", user_id: 1)
-long = Route.create(transportation: "airplane", distance: 700, name: "Can Tho to Hoi An", user_id: 1)
+# short = Route.create(transportation: "moto", distance: 200, name: "Can Tho to Ho Chi Minh", user_id: 1)
+# long = Route.create(transportation: "airplane", distance: 700, name: "Can Tho to Hoi An", user_id: 1)
+
+allUsers = {
+    "users": [
+        {
+            "username": "arnie",
+            "email": "arnie@example.com",
+            "password": "example"
+        },
+        {
+            "username": "janey",
+            "email": "janey@example.com",
+            "password": "example"
+        },
+        {
+            "username": "dougie",
+            "email": "dougie@example.com",
+            "password": "example"
+        }
+    ]
+}
+
+allRoutes = {
+    "routes": [
+        {
+            "transportation": "airplane",
+            "distance": 700,
+            "name": "Can Tho to Ho Chi Minh",
+            "user_id": 1
+        },
+        {
+            "transportation": "moto",
+            "distance": 200,
+            "name": "Can Tho to Hoi An",
+            "user_id": 1
+        }
+    ]
+}
+
+allUsers[:users].each do |user|
+    User.create(user)
+end
+
+allRoutes[:routes].each do |route|
+    Route.create(route)
+end
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // constructor() {
+  //   super()
+  //   state = {
+  //     users: []
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/users')
+  //   .then(resp => resp.json())
+  //   .then(this.setState({users: users}))
+  // }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
